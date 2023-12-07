3f4ed5469f709fd9537c39eece23c9e5ab2da14f
alzseven <jhp7140@gmail.com>
2023-12-07 23:31:44 +0900

    Continuous SendMsg

--- cpp_IOCP/SimpleIOCP/EchoServer.h
+++ cpp_IOCP/SimpleIOCP/EchoServer.h
@@ -1,9 +1,17 @@
 #pragma once
 
 #include "IOCPServer.h"
+#include "Packet.h"
+
+#include <vector>
+#include <deque>
+#include <thread>
+#include <mutex>
+
 
 class EchoServer : public IOCPServer
 {
+public:
 	virtual void OnConnect(const UINT32 clientIndex_) override
 	{
 		printf("[OnConnect] 클라이언트: Index(%d)\n", clientIndex_);
@@ -17,5 +25,73 @@ class EchoServer : public IOCPServer
 	virtual void OnReceive(const UINT32 clientIndex_, const UINT32 size_, char* pData_) override
 	{
 		printf("[OnReceive] 클라이언트: Index(%d), dataSize(%d)\n", clientIndex_, size_);
+
+		PacketData packet;
+		packet.Set(clientIndex_, size_, pData_);
+
+		std::lock_guard<std::mutex> guard(mLock);
+		mPacketDataQueue.push_back(packet);
 	}
+
+	void Run(const UINT32 maxClient)
+	{
+		mIsRunProcessThread = true;
+		mProcessThread = std::thread([this]() { ProcessPacket(); });
+
+		StartServer(maxClient);
+	}
+
+	void End()
+	{
+		mIsRunProcessThread = false;
+
+		if (mProcessThread.joinable())
+		{
+			mProcessThread.join();
+		}
+
+		DestroyThread();
+	}
+
+private:
+	void ProcessPacket()
+	{
+		while (mIsRunProcessThread)
+		{
+			auto packetData = DequePacketData();
+			if (packetData.DataSize != 0)
+			{
+				SendMsg(packetData.SessionIndex, packetData.DataSize, packetData.pPacketData);
+			}
+			else {
+				std::this_thread::sleep_for(std::chrono::microseconds(1));
+			}
+
+		}
+	}
+
+	PacketData DequePacketData()
+	{
+		PacketData packetData;
+
+		std::lock_guard<std::mutex> guard(mLock);
+		if (mPacketDataQueue.empty())
+		{
+			return PacketData();
+		}
+
+		packetData.Set(mPacketDataQueue.front());
+
+		mPacketDataQueue.front().Release();
+		mPacketDataQueue.pop_front();
+
+		return packetData;
+	}
+
+	bool mIsRunProcessThread = false;
+
+	std::thread mProcessThread;
+
+	std::mutex mLock;
+	std::deque<PacketData> mPacketDataQueue;
 };
